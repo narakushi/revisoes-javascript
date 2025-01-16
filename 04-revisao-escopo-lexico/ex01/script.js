2351fbcf6cd5b3cc89a6200d7685b23c3566d5aa
narakushi <taina.santana1305@gmail.com>
2025-01-15 21:13:04 -0300

    resolvendo questão 1 - escopo lexico

---
+++ 04-revisao-escopo-lexico/ex01/script.js
@@ -0,0 +1,33 @@
+/** testando uso de variavel declarada com var dentro de funções */
+/*
+const funcaoA = () => {
+    var numero = 0;
+
+    console.log(numero);
+}
+
+console.log(numero);*/
+
+/*
+1.	Escopo Léxico e Funções Aninhadas 
+Crie uma função counter que tenha uma variável count e retorne uma função increment. 
+A função increment deve aumentar o valor de count em 1 cada vez que for chamada. 
+Mostre que o escopo léxico permite que a função interna acesse a variável count.
+Dica: Use uma função aninhada para acessar o valor de count.*/
+
+const counter = () => {
+    let count = 0;
+
+   function increment() {
+        ++count;
+    }
+
+    increment();
+    increment();
+
+    return count;
+}
+
+console.log(counter());
+
+
