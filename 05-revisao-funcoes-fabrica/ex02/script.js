92114e44cae8354c94a01473c4b6783b3a7d80db
narakushi <taina.santana1305@gmail.com>
2025-03-06 19:15:22 -0300

    criando classe de contador

--- 05-revisao-funcoes-fabrica/ex02/script.js
+++ 05-revisao-funcoes-fabrica/ex02/script.js
@@ -2,47 +2,79 @@
 // Crie uma factory function chamada createCounter que retorna 
 // um objeto com os métodos increment, decrement e getCount. O contador deve começar em zero.
 
-// const createCounter = () => {
-//   let count = 0;
-
-//   return {
-//     getCount: () => count,
-
-//     increment() {
-//       count++;
-//     },
-
-//     decrement() {
-//       count--;
-//     }
-//   }
-// }
-
-// const count1 = createCounter();
-// const count2 = createCounter();
-
-// count1.increment();
-// count1.increment();
-
-// count2.increment();
-
-// console.log(count1.getCount());
-// console.log(count2.getCount());
-
+//Primeira Forma
 const createCounter = () => {
+  let count = 0;
+
   return {
-    count: 0,
+    getCount: () => count,
 
     increment() {
-      this.count++;
+      count++;
+    },
+
+    decrement() {
+      count--;
     }
   }
 }
 
 const count1 = createCounter();
+const count2 = createCounter();
 
 count1.increment();
 count1.increment();
-count1.increment();
 
-console.log(count1.count)
+count2.increment();
+
+console.log(count1.getCount());
+console.log(count2.getCount());
+
+//Segunda forma
+// const createCounter = () => {
+//   return {
+//     count: 0,
+
+//     increment() {
+//       this.count++;
+//     }
+//   }
+// }
+
+// const count1 = createCounter();
+
+// count1.increment();
+// count1.increment();
+// count1.increment();
+
+// console.log(count1.count)
+
+//Terceira forma
+
+// function CreateCounter() {
+//   this.count = 0;
+
+//   return {
+//     increment: () => {
+//       this.count++;
+//     }, 
+//     decrement: () => {
+//       this.count--;
+//     },
+//     getCounter: () => this.count
+//   }
+// }
+
+// const count1 = new CreateCounter();
+// const count2 = new CreateCounter();
+
+// count1.increment();
+// count1.increment();
+// count1.increment();
+
+// count2.increment();
+
+// count1.decrement();
+
+// console.log(count1.getCounter());
+// console.log(count2.getCounter());
