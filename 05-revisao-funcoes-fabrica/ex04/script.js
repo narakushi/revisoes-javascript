5435c4b9ac27945ee2e67fad7dbbc7291497895c
narakushi <taina.santana1305@gmail.com>
2025-03-11 21:14:42 -0300

    adicionando verificações ao codigo

--- 05-revisao-funcoes-fabrica/ex04/script.js
+++ 05-revisao-funcoes-fabrica/ex04/script.js
@@ -1,28 +1,41 @@
 // 4. Banco de Dados Simples
 // Crie uma factory function chamada createDatabase que retorne um objeto com métodos para adicionar, remover e listar itens (strings).
 
-function createDatabase () {
+function createDatabase() {
   let items = [];
 
   return {
     adicionar: (item) => {
-      if(typeof item != 'string'){
+      if (typeof item != 'string' || (!item.trim())) {
         console.log('Item deve ser um nome');
         return;
       }
 
-      items.push(item);
-      console.log(`${item} adicionado(a) a lista de itens`);
+      let indice = items.findIndex((currentItem) => currentItem.toLowerCase() === item.toLowerCase());
+      
+      if(indice === -1){
+        item = item.trim();
+        items.push(item);
+        console.log(`${item} adicionado(a) a lista de itens`);
+      }
+   
     },
 
     remover: (item) => {
-      items = items.filter((currentItem) => currentItem.toLowerCase() != item.toLowerCase());
-      console.log(`${item} foi removido do registro de items`);
+      let i = items.findIndex((currentItem) => currentItem.toLowerCase() === item.toLowerCase());
+
+      if(i === -1){
+        console.log(`${item} não encontrado(a) na lista`);
+        return;
+      }
+
+      items = items.filter((currentItem) => currentItem.toLowerCase() !== item.toLowerCase());
+      console.log(`${item} removido da lista!`);
     },
 
     listar: () => {
-      console.log("*******Lista de Items*******");
-      items.length != 0 ? items.map((item) => console.log(item)) : console.log('Lista vazia!');
+      console.log("******Lista de Items******");
+      items.length != 0 ? items.forEach((item) => console.log(item)) : console.log('Lista vazia!');
     }
   }
 }
@@ -36,9 +49,9 @@ db.adicionar('Caderno');
 db.remover('mochila');
 
 db.adicionar('Estojo');
+db.adicionar('estojo');
+db.remover('caneta');
 
-db.remover('Lapis');
-db.remover('caderno');
-db.remover('estojo');
 
 db.listar();
+
